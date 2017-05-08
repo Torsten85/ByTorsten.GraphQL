c66317c1130672b4cb795182679647cab7f27929
Torsten Blindert <info@by-torsten.com>
2017-05-08 21:47:00 +0200

    Removed unused security experiment

--- Classes/Security/Authorization/Privilege/Resolver/ResolverPrivilegeInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace ByTorsten\GraphQL\Security\Authorization\Privilege\Resolver;
-
-use Neos\Flow\Security\Authorization\Privilege\PrivilegeInterface;
-
-interface ResolverPrivilegeInterface extends PrivilegeInterface
-{
-}
